d2cb64ac940ff59194b73acd6b64a8babb835cc6
Clara Heinz <clarakheinz@gmail.com>
2022-08-26 17:30:53 -0400

    resetting up the backend cause i accidentally deleted it

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const express = require("express");
 const cors = require("cors");
 
 //set mongodb connection
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://codepalousa:<password>@cluster0.9gn4rfd.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = "mongodb+srv://codepalousa:codepalousa@cluster0.9gn4rfd.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+let collection;
 client.connect(err => {
-  const collection = client.db("todos").collection("todos");
+    collection = client.db("todos").collection("todos");
   // perform actions on the collection object
 //   client.close(); we do not need to close connection right now
 // good standard is to have mongo connection on each page where we access the db but this app is small for now
@@ -29,13 +30,63 @@ app.use(express.json());
 app.listen(PORT, ()=> console.log(`App started and listening on port ${PORT}`));
 
 // add get endpoint for querying all todos
+app.get("/todos", async(req, res)=>{
+    let todos = await collection.find({}).toArray();
+    res.send(todos).status(200);
+    console.log(todos);
+});
 
-//add post endpoint to post new todo to database
+// add post endpoint to post new todo to database
+app.post("/todos", async (req, res) =>{
+    try {
+        res.send(await collection.insertOne({description: req.body})).status(201);
+    }
+    catch(e) {
+        console.log(e);
+        res.send(500);
+    }
+});
+
+// get a todo by id
+app.get("/todos/:id", async (req, res) => {
+    try {
+        res.send(
+            await collection.findOne({_id: ObjectId(req.params)}))
+            .status(200);
+    }
+    catch(e) {
+        console.log(e);
+        res.send(500);
+    }
+});
 
 // add put endpoint to update single todo
+app.put("/todos/:id", async (req, res) => {
+    try {
+        console.log(req.params);
+        res.send(
+            await collection.findOneAndUpdate({_id: ObjectId(req.params.id)}, {isDone: req.body}))
+            .status(204);
+    }
+    catch(e) {
+        console.log(e);
+        res.send(500);
+    }
+});
 
 //add delete endpoint
-
+app.delete("todos/:id", async (req, res) => {
+    try{
+        console.log(req.params)
+        res.send(
+            await collection.findOneAndDelete({_id: ObjectId(req.params.id)})
+        ).send(200);
+    }
+    catch(e){
+        console.log(e);
+        res.send(500);
+    }
+})
 
 
 
